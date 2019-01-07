ac9585aec777203c192c23495dc5d4466576c575
Michael Schwarze <michael@schwarze-web.de>
2019-01-07 11:43:50 +0100

    Improves Mountebank integration.

--- test/sensor_test.rb
+++ test/sensor_test.rb
@@ -4,33 +4,34 @@ require_relative 'test_helper'
 
 # Test class for Minitest Unit Tests
 class SensorTest < Minitest::Test
-  def setup
-    # ...
-  end
+  MOUNTEBANK_ADDRESS = 'http://localhost:2525/imposters/'.freeze
 
   def mountebank
     # Assumes that [Mountebank][http://www.mbtest.org] is up an running
-    # TODO: Include Mountebank dependency
     # ---
     # First, delete any old imposters
     mountebank_delete
 
     # Then, setup imposter needed for testing
     mountebank_setup
-  rescue StandardError => exception
-    puts exception
   end
 
   def mountebank_delete
-    HTTParty.delete('http://localhost:2525/imposters/1000')
+    HTTParty.delete(MOUNTEBANK_ADDRESS)
+  rescue StandardError => errormsg
+    LOGGER.warn "Couldn't delete from Mountebank@#{MOUNTEBANK_ADDRESS}!\n#{errormsg}"
+    raise
   end
 
   def mountebank_setup
-    body = <<-HEREDOC
-    {"port": 1000, "protocol": "http", "name": "ESP32-FC37", "stubs": [{"predicates": [{"equals": {"method": "GET", "path": "/"}}], "responses": [{"is": {"statusCode": 200, "headers": {"Content-Type": "application/json"}, "body": {"ESP32": {"status": {"uptime": "1", "LED_builtin": "off"}, "sensor": {"type": "FC37", "value": "4095", "interpretation": "dry"}}}}}]}]}
+    body = <<~HEREDOC
+      {"port": 1000, "protocol": "http", "name": "ESP32-FC37", "stubs": [{"predicates": [{"equals": {"method": "GET", "path": "/"}}], "responses": [{"is": {"statusCode": 200, "headers": {"Content-Type": "application/json"}, "body": {"ESP32": {"status": {"uptime": "1", "LED_builtin": "off"}, "sensor": {"type": "FC37", "value": "4095", "interpretation": "dry"}}}}}]}]}
     HEREDOC
 
-    HTTParty.post('http://localhost:2525/imposters/', body: body, headers: { 'Content-Type' => 'application/json' })
+    HTTParty.post(MOUNTEBANK_ADDRESS, body: body, headers: { 'Content-Type' => 'application/json' })
+  rescue StandardError => errormsg
+    LOGGER.warn "Couldn't post to Mountebank@#{MOUNTEBANK_ADDRESS}!\n#{errormsg}"
+    raise
   end
 
   def test_that_it_has_a_version_number
